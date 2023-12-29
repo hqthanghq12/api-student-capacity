9b1e11626f5c2db5e792d3b63b52d7e44c8cec73
dinhtv7 <nguyenduyquangvinh2906@gmail.com>
2023-12-29 09:57:54 +0700

    update

--- resources/views/pages/poetry/playtopic/result.blade.php
+++ resources/views/pages/poetry/playtopic/result.blade.php
@@ -187,6 +187,7 @@
                                             @php
                                                 $stt = 'A';
                                                 $question = $questions->where('id', $questionId)->first();
+                                                @dd($question->images);
                                                 $images = $question->images ? $question->images->toArray() : [];
                                                 $index = $key + 1;
                                                 $correctAnswer = $question->answers->where('is_correct', 1)->first();
