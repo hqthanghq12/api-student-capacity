9561ea425317a74e40b309c01d98b8bc04559bce
Nguyen huu phu <phub3@gmail.com>
2025-03-15 21:22:28 +0700

    update mathjax

--- resources/views/pages/subjects/question/list.blade.php
+++ resources/views/pages/subjects/question/list.blade.php
@@ -506,9 +506,12 @@
 
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 {{--    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>--}}
-<script type="text/javascript" async src="assets/js/custom/apps/mathjax/mathjax/es5/tex-mml-chtml.js"></script>
+{{-- <script type="text/javascript" async src="assets/js/custom/apps/mathjax/mathjax/es5/tex-mml-chtml.js"></script> --}}
+<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
-    <script id="MathJax-script" async src="assets/js/custom/apps/mathjax/tex-mml-chtml.js"></script>
+
+    {{-- <script id="MathJax-script" async src="assets/js/custom/apps/mathjax/tex-mml-chtml.js"></script> --}}
     <script>
 
         function notify(message) {
