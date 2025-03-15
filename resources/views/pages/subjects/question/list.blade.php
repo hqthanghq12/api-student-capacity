ff34ace5160d417637e3d79697661374baa3cc98
Nguyen huu phu <phub3@gmail.com>
2025-03-15 14:52:43 +0700

    update mathjax

--- resources/views/pages/subjects/question/list.blade.php
+++ resources/views/pages/subjects/question/list.blade.php
@@ -506,6 +506,8 @@
 
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 {{--    <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>--}}
+<script type="text/javascript" async src="assets/js/custom/apps/mathjax/mathjax/es5/tex-mml-chtml.js"></script>
+
     <script id="MathJax-script" async src="assets/js/custom/apps/mathjax/tex-mml-chtml.js"></script>
     <script>
 
